3121c3617227586474a2a867cb3a4c3ee75c1646
Rafael Silva <rafaelmike33@gmail.com>
2023-12-02 21:36:59 -0300

    att

--- Projetos/PortifolioJS/assets/js/main.js
+++ Projetos/PortifolioJS/assets/js/main.js
@@ -1,7 +1,25 @@
 
+function updateProfileData(profileData) {
+    const name = document.getElementById('profile.name');
+    name.innerText = profileData.name;
+
+    const job = document.getElementById('profile.job');
+    job.innerText = profileData.job;
+
+    const location = document.getElementById('profile.location');
+    location.innerText = profileData.location;
+
+    const phone = document.getElementById('profile.phone');
+    phone.innerText = profileData.phone;
+    phone.href = `https://wa.me/55${profileData.phone}`;
+
+
+
+
+}
 
 (async () => {
 
     const profileData = await fetchProfileData();
-    console.log(profileData);
+    updateProfileData(profileData);
 })();
