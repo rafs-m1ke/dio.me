969395273499bb478add6971ce9c4a256b8f1f36
Rafael Silva <130245184+rafs-m1ke@users.noreply.github.com>
2024-02-10 19:55:46 -0300

    desafio banco

---
+++ formacaoPythonDev/string_1.py
@@ -0,0 +1,17 @@
+nome = "Rafael"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "    Olá mundo!  "
+
+print(texto.strip())
+print(texto.lstrip())
+print(texto.rstrip())
+
+print(".".join(nome))
+
+string = "Python"
+
+print(string.center(20, "#"))
---
+++ formacaoPythonDev/string_2.py
@@ -0,0 +1,9 @@
+nome = "Rafael"
+idade = 24
+profissao = "Militar"
+linguagem = "Python"
+
+print("Nome: %s Idade: %d" % (nome,idade))
+print("Nome: {} Idade: {}".format(nome,idade))
+print("Nome: {0} Idade: {1}".format(nome,idade))
+print("Nome: {0} Idade: {1}".format(nome,idade))
